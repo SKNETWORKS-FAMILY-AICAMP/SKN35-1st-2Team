f350ca0144d6f5242489ceb2462692efadd774ec
chan <pcr0807@naver.com>
2026-07-25 21:18:57 +0900

    [Add] service_center sql

---
+++ database/service_center.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE car_recall; -- 데이터베이스 미생성 시 실행
+
+USE car_recall; -- 데이터베이스 선택
+
+SELECT DATABASE(); -- car_recall 데이터베이스 선택 확인
+
+-- 서비스 센터 테이블
+CREATE TABLE service_center (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL COMMENT '서비스센터명',
+    address VARCHAR(255) NOT NULL COMMENT '주소',
+    phone VARCHAR(30) COMMENT '전화번호',
+    latitude DECIMAL(10,8) NOT NULL COMMENT '위도',
+    longitude DECIMAL(11,8) NOT NULL COMMENT '경도',
+    company VARCHAR(10) NOT NULL COMMENT '제조사',
+    INDEX idx_name (name)
+) ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_unicode_ci;
+
+-- 법정동 테이블
+CREATE TABLE legal_dong (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    sido_name VARCHAR(30) NOT NULL COMMENT '시도명',
+    sigungu_name VARCHAR(30) NULL COMMENT '시군구명',
+    eupmyeondong_name VARCHAR(50) NULL COMMENT '읍면동명',
+    ri_name VARCHAR(50) NULL COMMENT '리명',
+
+    INDEX idx_sido (sido_name),
+    INDEX idx_sigungu (sigungu_name),
+    INDEX idx_eupmyeondong (eupmyeondong_name)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_unicode_ci;
+
+-- 생성된 테이블 확인
+SHOW TABLES;
